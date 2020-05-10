9da208437f1e787b502378545ce2aa8f58820d96
Shashwat Shivam <shivamshaswat@gmail.com>
2020-05-11 02:46:50 +0530

    Setting up repository

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Auth Server !!');
 });
 
-app.listen(process.env["PORT"] , () => {
-    console.log(`Server listening on ${process.env["PORT"]}!`)
+app.listen(process.env.PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server listening on ${process.env.PORT}!`);
 });
