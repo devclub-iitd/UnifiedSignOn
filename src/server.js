547bcf7b373c9bc9672ce8c7874008a519597d25
Jatin Prakash <jatinprakash1511@gmail.com>
2020-05-12 13:30:01 +0530

    make errors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
-const express = require('express');
-
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Auth Server !!');
+    res.send('Welcome to the Auth Server !!')
 });
 
 app.listen(process.env.PORT, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server listening on ${process.env.PORT}!`);
+    console.log(`Server listening on ${process.env.PORT}!`)
 });
