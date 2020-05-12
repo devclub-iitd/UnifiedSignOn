7a2d20db3ff2088db1fcdf6e9dc627780982e614
Aryan <aryanguptaleo@gmail.com>
2020-05-12 09:12:31 +0530

    Added Register Page

--- src/server.js
+++ src/server.js
@@ -13,6 +13,9 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login');
 });
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 app.listen(process.env.PORT, () => {
     // eslint-disable-next-line no-console
