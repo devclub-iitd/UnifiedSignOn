bbfc274a538a99648e49ea9fda47a9d461fa5abc
Harsh Agrawal <harsh.ag14901@gmail.com>
2020-05-16 17:54:43 +0530

    Refresh of access tokens and remember me tokens implemented for auth.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,1 +1,3 @@
 export const secretkey = 'mysecretkey';
+export const expTime = 1000 * 60 * 20;
+export const rememberTime = 1000 * 60 * 60 * 24 * 2;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,34 +1,83 @@
 import express from 'express';
-import { verify } from 'jsonwebtoken';
-import { secretkey } from '../config/keys';
+import jwt, { verify } from 'jsonwebtoken';
+import { secretkey, expTime, rememberTime } from '../config/keys';
 
 const router = express.Router();
 
-// post route to check validity of tokens, clients will hit this route.
-router.post('/', (req, res) => {
-    // extract token from cookie
-    const token = req.header('auth-token');
+function createJWTCookie(user, res, tokenName = 'token') {
+    const payload = {
+        user: {
+            id: user._id,
+            email: user.email,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            username: user.username,
+            role: user.role,
+        },
+    };
+    const exp = tokenName === 'rememberme' ? rememberTime : expTime;
+    // create a token
+    const token = jwt.sign(payload, secretkey, {
+        expiresIn: exp, // in ms
+        issuer: 'auth.devclub.in',
+    });
 
-    if (!token) {
-        return res.status(401).json({ msg: 'Error, token is not present' });
-    }
+    // set the cookie with token with the same age as that of token
+    res.cookie(tokenName, token, {
+        maxAge: exp, // in ms
+        secure: false, // set to true if your using https
+        httpOnly: true,
+        sameSite: 'lax',
+    });
+}
 
-    // So the token is present, so lets verify it
+function verifyToken(token, res, tokenName = 'token') {
     try {
         const decoded = verify(token, secretkey);
-
         const { user } = decoded;
 
-        return res.status(200).json({ user });
+        // Set a new cookie which will extend the session a further {expTime} amount of time.
+        // So essentially whenever any auth request is made the user session will be extended.
+        if (tokenName === 'rememberme') {
+            // Refresh the remember me cookie
+            createJWTCookie(user, res, tokenName);
+        }
+
+        // Refresh the token cookie
+        createJWTCookie(user, res);
+
+        return res.status(200).json({
+            user,
+        });
     } catch (err) {
         // I wasn't able to verify the token as it was invalid
-        // clear the token, but somehow this doesn't work, so just to
-        // be safe, do the same at client server as well.
-        res.clearCookie('token');
+        // clear the token
+        res.clearCookie(tokenName);
 
         // now send a response
-        return res.status(401).json({ msg: 'Error, token not valid' });
+        return res.status(401).json({
+            msg: 'Error, token not valid',
+        });
     }
+}
+
+// post route to check validity of tokens, clients will hit this route.
+router.post('/verify', (req, res) => {
+    // extract token from cookie
+    const { token, rememberme } = req.cookies;
+
+    if (!token) {
+        if (!rememberme) {
+            return res.status(401).json({
+                msg: 'Error, token is not present',
+            });
+        }
+
+        return verifyToken(rememberme, res, 'rememberme');
+    }
+
+    return verifyToken(token, res);
 });
 
 export default router;
+export { createJWTCookie };
