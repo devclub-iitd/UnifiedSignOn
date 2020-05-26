b5142cf62370553c14e41ac5415ce4d1ce3dfa8c
Vishal Asthana <58475949+CrypticPoet@users.noreply.github.com>
2020-05-26 20:33:03 +0530

    Update form layout and some functionality changes
    Password is not required anymore to update personal info fields

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -18,62 +18,58 @@ router.post('/', async (req, res) => {
         newUsername,
         newPassword,
     } = req.body;
-    // Check if user has entered password
-    if (password) {
-        // Find the current user from the database
-        try {
-            const user = await User.findOne({ email });
-            const isMatch = await bcrypt.compare(password, user.password);
-            if (!isMatch) {
-                res.render('settings', {
-                    message: 'Incorrect Password',
+
+    try {
+        const user = await User.findOne({ email });
+        if (newPassword) {
+            if (password) {
+                const isMatch = await bcrypt.compare(password, user.password);
+                if (!isMatch) {
+                    return res.render('settings', {
+                        message: 'Incorrect Password',
+                        error: true,
+                    });
+                }
+                const passwordHash = await bcrypt.hash(newPassword, 10);
+                user.password = passwordHash;
+            } else {
+                return res.render('settings', {
+                    message:
+                        'Please enter your old password to update your password',
                     error: true,
                 });
-            } else {
-                // Update changed fields
-                if (firstName) {
-                    user.firstname = firstName;
-                }
-                if (lastName) {
-                    user.lastname = lastName;
-                }
-                if (newUsername) {
-                    user.username = newUsername;
-                }
-                if (newPassword) {
-                    const hash = await bcrypt.hash(newPassword, 10);
-                    user.password = hash;
-                }
+            }
+        }
 
-                // Save the user to take advantage of inbuilt validation
-                await user.save({}, (err) => {
-                    // Display an error message if their
-                    if (err) {
-                        res.render('settings', {
-                            // TODO : Filter the error messages based on error code and display a user-friendly message
-                            message: err.message,
-                            error: true,
-                        });
-                    } else {
-                        // Clear old JWT and create a new one based on the updated information
-                        res.clearCookie(accessTokenName);
-                        createJWTCookie(user, res);
-                        res.render('settings', {
-                            message: 'Fields updated successfully',
-                            error: false,
-                        });
-                    }
+        if (firstName) {
+            user.firstname = firstName;
+        }
+        if (lastName) {
+            user.lastname = lastName;
+        }
+        if (newUsername) {
+            user.username = newUsername;
+        }
+
+        await user.save({}, (err) => {
+            if (err) {
+                console.log(err);
+                return res.render('settings', {
+                    message: err.message,
+                    error: true,
                 });
             }
-        } catch (error) {
+            res.clearCookie(accessTokenName);
+            createJWTCookie(user, res);
             res.render('settings', {
-                message: 'Seems like an error occured!',
-                error: true,
+                message: 'Fields updated successfully',
+                error: false,
             });
-        }
-    } else {
+        });
+    } catch (error) {
+        console.log(error);
         res.render('settings', {
-            message: 'Please enter your password to modify fields',
+            message: 'Seems like an error occurred',
             error: true,
         });
     }
