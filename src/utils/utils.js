9d4ae313d01a04e82ff2800661a4161584978ff9
Harsh Agrawal <55060849+Harsh14901@users.noreply.github.com>
2020-08-01 07:33:05 +0530

    Some bug fixes in social auth and Privacy Policy added. (#62)
    * Refresh of access tokens and remember me tokens implemented for auth.js
    
    * Remember Me tokens are considered for profile.js too. Repetitive Code eliminated.
    
    * added helper functions to utils.js
    
    * Token name are now not hardcoded
    
    * Changed signing algorithm from HS256 to RS256
    
    * Changed verify-token to refresh-token
    
    * Variable names changed and some minor fixes
    
    * Basic code for google Auth added
    
    * Github and Facebook integration done
    
    * Social auth accounts are now linked with mongodb primary accounts
    
    * Code Refactored
    
    * Code ready for merge
    
    * Code for iitd kerberos auth implemented but not yet tested
    
    * Social auth changes
    A confirm page is loaded before creating a new account using social auth.
    
    * Fixed the Zombie account bug and serviceURL redirection added
    
    * Social Accounts of different email addresses can now be linked, Some code optimizations
    
    * Access Control Model improved and Schemas for client, role added
    
    * Backend functionality for registration of clients implemented
    
    * Added a post route to update roles for a client
    
    * Update utils.js
    
    * Added Clients List Page
    
    * Added Client Registration and Management frontend
    
    The client registration and management frontend is now fully functional. Users with appropriate privilege can register domains and create custom roles and update them if necessary.
    
    * Minor Bug fixes
    
    * Insert and delete of roles in update handler added
    
    * Added ability to add and delete roles in the client management page
    
    Also fixed a minor bug which caused deletion of some roles that the user wasn't even assigned.
    
    * Some fixes and optimisations
    
    * Added messages and a few other changes
    
    * Added endpoints to download public key and config vars
    
    * Minor changes
    
    * Links for config and publicKey added
    
    * Fixed a critical security vulnerability
    
    * Production changes
    
    * Fixed bug in logout and social_auth
    
    * Trying to fix proxy issues
    
    * Fixing social auth, configured proxy settings
    
    * Some bug fixes in social_auth, privacy policy added
    
    Co-authored-by: Vishal Asthana <58475949+CrypticPoet@users.noreply.github.com>

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -130,7 +130,7 @@ const sendVerificationEmail = async (user) => {
             issuer: keys.iss,
             algorithm: 'RS256',
         });
-        const response = await axios.post(process.env.MAILER_URL, {
+        await axios.post(process.env.MAILER_URL, {
             to: user.email,
             subject: 'Account Verification',
             html: `<h3>Click here to verify your account</h3>
@@ -139,7 +139,6 @@ const sendVerificationEmail = async (user) => {
               </p>`,
             secret: process.env.MAILER_TOKEN,
         });
-        console.log(response.data);
     } catch (error) {
         console.log(error);
     }
@@ -156,7 +155,7 @@ const sendPassResetEmail = async (user, newPass) => {
             issuer: keys.iss,
             algorithm: 'RS256',
         });
-        const response = await axios.post(process.env.MAILER_URL, {
+        await axios.post(process.env.MAILER_URL, {
             to: user.email,
             subject: 'Password Reset',
             html: `<h3>Click here to reset password for your account</h3>
@@ -165,7 +164,6 @@ const sendPassResetEmail = async (user, newPass) => {
               </p>`,
             secret: process.env.MAILER_TOKEN,
         });
-        console.log(response.data);
     } catch (error) {
         console.log(error);
     }
